2ff35dc3a1e0e0a7b37f7be9b00ac66390152eac
giftdivine <giftdivine787@gmail.com>
2026-07-20 14:40:05 +0100

    Dom

---
+++ intro to dom/script.js
@@ -0,0 +1,10 @@
+const title = document.getElementById("title");
+title.style.color = "red";
+title.style.fontSize = "100px"
+title.style.backgroundColor = "blue"; 
+title.style.borderRadius = "1rem";
+
+const Image = document.getElementById("img");
+Image.style.backgroundColor = "red";
+Image.style.borderRadius = "1rem";
+// Image.src = "profile.jpg";
